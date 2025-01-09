461827ee722328a57d27fc0ac74ff055a68c0791
Probe1st <svintsitskiy.leha@mail.ru>
2025-01-09 14:22:54 +0500

    grade 1. stage 1: task 1 adding greetings.py

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "John"
+title = "Shopping"
+content = "apples, oranges, grapes"
+status = "open"
+created_date = "10.10.24"
+issue_date = "11.10.24"
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
