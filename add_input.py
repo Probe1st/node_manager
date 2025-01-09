dbe92437daf92eac1b77f6103636fc5aa286e7bc
Probe1st <svintsitskiy.leha@mail.ru>
2025-01-09 14:40:58 +0500

    grade 1. stage 1: task 3 adding add_input.py

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username = input("Enter an username: ")
+title = input("Enter a title: ")
+content = input("Enter a content: ")
+status = input("Enter a status: ")
+created_date = input("Enter a created date in format dd-mm-yy: ")
+issue_date = input("Enter a issue date in format dd-mm-yy: ")
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date[:-3])
+print(issue_date[:-3])
